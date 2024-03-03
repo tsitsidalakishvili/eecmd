471276887dd0be74c80c53f10601e8e1c13df617
tsitsidalakishvili <114109718+tsitsidalakishvili@users.noreply.github.com>
2024-03-03 23:35:53 +0400

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from io import BytesIO
 from langchain.document_loaders import UnstructuredPDFLoader, PyPDFLoader, AssemblyAIAudioTranscriptLoader
 import requests
 import pandas as pd
+from langchain.memory import ConversationBufferMemory
 
 import os
 from dotenv import load_dotenv
